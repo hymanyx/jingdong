c8b63fec9ff2663fb7214e9fc8a47368e8ea113a
hymanyx <282209415@qq.com>
2016-10-28 18:56:14 +0800

    完善了详情页采集，价格及评论采集

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
-process.crawl('product_spider')
+# process.crawl('category_spider')
 # process.crawl('log_spider')
 # process.crawl('cps_spider')
 process.start()
